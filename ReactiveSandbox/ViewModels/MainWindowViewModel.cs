8a1ef7e175ee1f8152333a78bf2eb18877fc84ad
Alex Tóth <alex.toth21@gmail.com>
2022-07-18 16:31:51 +0200

    remove blank line

--- ReactiveSandbox/ViewModels/MainWindowViewModel.cs
+++ ReactiveSandbox/ViewModels/MainWindowViewModel.cs
@@ -23,7 +23,6 @@ public class MainWindowViewModel : ReactiveValidationObject, IDisposable
     private readonly Regex _emailRegex = new("^((\"[\\w-\\s]+\")|([\\w-]+(?:\\.[\\w-]+)*)|(\"[\\w-\\s]+\")([\\w-]+(?:\\.[\\w-]+)*))(@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$)|(@\\[?((25[0-5]\\.|2[0-4][0-9]\\.|1[0-9]{2}\\.|[0-9]{1,2}\\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\\]?$)", RegexOptions.Compiled);
     private bool _disposedValue;
 
-
     [Reactive]
     public string Email { get; set; } = string.Empty;
 
