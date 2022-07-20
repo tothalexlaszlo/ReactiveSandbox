537e0d6db78e49b06bf717e8afb7c2c712e7c4b7
Adam Radocz <adam.radocz@gmail.com>
2022-07-20 20:27:19 +0200

    Wpf opens command line too.

---
+++ ReactiveSandbox.Wpf/ValidationErrorsToStringConverter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ReativeSandbox.Wpf;
+
+/// <summary>
+/// Segít elkerülni, hogyha nincs validálási hiba, akkor a 
+/// {Binding RelativeSource={RelativeSource Self},Path=(Validation.Errors)[0].ErrorContent}
+/// binding hibára fusson:
+/// Cannot get ‘Item[]‘ value (type ‘ValidationError’) from ‘(Validation.Errors)’
+/// 
+/// innen: https://wpftutorial.net/DataValidation.html
+/// </summary>
+public class ValidationErrorsToStringConverter : MarkupExtension, IValueConverter
+{
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return new ValidationErrorsToStringConverter();
+    }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ReadOnlyObservableCollection<ValidationError> errors)
+            return string.Join(Environment.NewLine, errors.Select(e => e.ErrorContent as string));
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
