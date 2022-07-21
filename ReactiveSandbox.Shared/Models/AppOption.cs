4f67cbfddba91f010665189e43a09a7f91d91e16
Adam Radocz <adam.radocz@gmail.com>
2022-07-21 22:32:30 +0200

    Slow down the sgenerator.

--- ReactiveSandbox.Shared/Models/AppOption.cs
+++ ReactiveSandbox.Shared/Models/AppOption.cs
@@ -2,8 +2,8 @@ namespace ReactiveSandbox.Shared.Models;
 
 public class AppOption
 {
-    public TimeSpan SendingInterval { get; set; } = TimeSpan.FromSeconds(0.2);
-    public TimeSpan FutureToleranceTime { get; set; } = TimeSpan.FromSeconds(0.4);
-    public TimeSpan InactiveToleranceTime { get; set; } = TimeSpan.FromSeconds(0.4);
-    public TimeSpan ExpiredToleranceTime { get; set; } = TimeSpan.FromSeconds(2);
+    public TimeSpan SendingInterval { get; set; } = TimeSpan.FromSeconds(1);
+    public TimeSpan FutureToleranceTime { get; set; } = TimeSpan.FromSeconds(2);
+    public TimeSpan InactiveToleranceTime { get; set; } = TimeSpan.FromSeconds(2);
+    public TimeSpan ExpiredToleranceTime { get; set; } = TimeSpan.FromSeconds(10);
 }
